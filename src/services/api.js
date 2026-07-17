dcf50a01c80f55c7264de3c019efed001e2c07d4
Aldair Rutte <xxpiltisxx@gmail.com>
2026-07-17 15:39:25 -0500

    se agregaron variables de entorno para el entorno de produccion

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-        baseURL: "",
+        baseURL: import.meta.env.VITE_API_URL,
         withCredentials: true
 })
--- src/services/funvalApi.js
+++ src/services/funvalApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://'
+const BASE_URL = import.meta.env.VITE_API_URL
 
 const prefijoApi='/api/v1'
 
